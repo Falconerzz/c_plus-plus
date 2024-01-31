48d379e886d1bd733f02c81c3d98d1a1825c1d6c
Falconer_zz <98586280+Falconerzz@users.noreply.github.com>
2024-01-31 09:44:25 +0700

    Add files via upload

---
+++ Week 11 - Functions/Exercise 1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int isValidID(string studentID) {
+    if (studentID.length() != 8) {
+        return 0;
+    }
+
+    char level = toupper(studentID[0]);
+    if (level != 'B' && level != 'M' && level != 'D') {
+        return 0;
+    }
+
+    int checksum = ((studentID[1] - '0') * 49 + (studentID[2] - '0') * 7 +
+                    (studentID[3] - '0') * 49 + (studentID[4] - '0') * 7 +
+                    (studentID[5] - '0') * 49 + (studentID[6] - '0') * 7) % 10;
+
+    int lastDigit = studentID[7] - '0';
+    if (checksum != lastDigit) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    string id[3];
+
+    for (int i = 0; i < 3; i++) {
+        cin >> id[i];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (isValidID(id[i])) {
+            cout << "valid" << endl;
+        } else {
+            cout << "invalid" << endl;
+        }
+    }
+
+    return 0;
+}
+
